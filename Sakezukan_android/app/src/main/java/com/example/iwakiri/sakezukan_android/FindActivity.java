21a9f20a291e829c3bafd79811a7e52b3537e60d
岩切　潤 <j.iwakiri@flama.co.jp>
2017-03-31 15:36:31 +0900

    FIND画面内の遷移条件確認

--- Sakezukan_android/app/src/main/java/com/example/iwakiri/sakezukan_android/FindActivity.java
+++ Sakezukan_android/app/src/main/java/com/example/iwakiri/sakezukan_android/FindActivity.java
@@ -5,38 +5,71 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class FindActivity extends AppCompatActivity {
 
-    Boolean isExist = true;
-    Boolean isFound = true;
+    Boolean isExist;
+    Boolean isFound;
     Button button;
+    EditText editText;
+    String str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find);
+
+        editText = (EditText) findViewById(R.id.edittext);
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                defineData();
                 searchData();
             }
         });
     }
 
+    private void defineData() {
+        isExist = null;
+        isFound = null;
+        str = null;
+
+        str = editText.getText().toString().trim();
+        switch (str) {
+            case "0":
+                isExist = true;
+                isFound = true;
+                break;
+            case "1":
+                isExist = true;
+                isFound = false;
+                break;
+            case "2":
+                isExist = false;
+                isFound = false;
+                break;
+        }
+    }
+
     private void searchData() {
-        if (isExist) {
-            if (isFound) {
-                Intent intent = new Intent(this, FindNewDataActivity.class);
-                startActivity(intent);
+        if (!str.isEmpty()) {
+            if (isExist) {
+                if (isFound) {
+                    Intent intent = new Intent(this, FindNewDataActivity.class);
+                    startActivity(intent);
+                } else {
+                    Intent intent = new Intent(this, FindFoundDataActivity.class);
+                    startActivity(intent);
+                }
             } else {
-                Intent intent = new Intent(this, FindFoundDataActivity.class);
+                Intent intent = new Intent(this, FindNoDataActivity.class);
                 startActivity(intent);
             }
         } else {
-            Intent intent = new Intent(this, FindNoDataActivity.class);
-            startActivity(intent);
+            Toast.makeText(getApplicationContext(), "0,1,2以外", Toast.LENGTH_SHORT).show();
         }
     }
 }
