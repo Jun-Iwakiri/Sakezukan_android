ac990af405c55668a4c4f084b3d608bce1f1e633
岩切　潤 <j.iwakiri@flama.co.jp>
2017-04-07 18:24:04 +0900

    ガイド画面から他画面遷移

--- Sakezukan_android/app/src/main/java/com/example/iwakiri/sakezukan_android/GuideActivity.java
+++ Sakezukan_android/app/src/main/java/com/example/iwakiri/sakezukan_android/GuideActivity.java
@@ -13,11 +13,11 @@ public class GuideActivity extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
 
-        Button homeButton = (Button) findViewById(R.id.button11);
-        Button guideButton = (Button) findViewById(R.id.button10);
-        Button tasteButton = (Button) findViewById(R.id.button9);
-        Button findButton = (Button) findViewById(R.id.button8);
-        Button helpButton = (Button) findViewById(R.id.button7);
+        Button homeButton = (Button) findViewById(R.id.button87);
+        Button guideButton = (Button) findViewById(R.id.button86);
+        Button tasteButton = (Button) findViewById(R.id.button85);
+        Button findButton = (Button) findViewById(R.id.button84);
+        Button helpButton = (Button) findViewById(R.id.button83);
         Button noTastedButton = (Button) findViewById(R.id.button76);
         Button tastedButton = (Button) findViewById(R.id.button77);
         homeButton.setOnClickListener(this);
@@ -32,36 +32,38 @@ public class GuideActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.button11:
+            case R.id.button87:
                 Intent homeIntent = new Intent(getApplicationContext(), HomeActivity.class);
                 startActivity(homeIntent);
                 finish();
                 break;
-            case R.id.button10:
+            case R.id.button86:
                 Intent guideIntent = new Intent(getApplicationContext(), GuideActivity.class);
                 startActivity(guideIntent);
                 finish();
                 break;
-            case R.id.button9:
+            case R.id.button85:
                 Intent tasteIntent = new Intent(getApplicationContext(), TasteActivity.class);
                 startActivity(tasteIntent);
                 finish();
                 break;
-            case R.id.button8:
+            case R.id.button84:
                 Intent findIntent = new Intent(getApplicationContext(), FindActivity.class);
                 startActivity(findIntent);
                 finish();
                 break;
-            case R.id.button7:
+            case R.id.button83:
                 Intent helpIntent = new Intent(getApplicationContext(), HelpActivity.class);
                 startActivity(helpIntent);
                 finish();
                 break;
             case R.id.button76:
-                Intent intent = new Intent(getApplicationContext(), GuideDetailActivity.class);
-                startActivity(intent);
+                Intent notTastedIntent = new Intent(getApplicationContext(), GuideDetailActivity.class);
+                startActivity(notTastedIntent);
                 break;
             case R.id.button77:
+                Intent TastedIntent = new Intent(getApplicationContext(), GuideDetailActivity.class);
+                startActivity(TastedIntent);
                 break;
         }
     }
--- Sakezukan_android/app/src/main/java/com/example/iwakiri/sakezukan_android/GuideDetailActivity.java
+++ Sakezukan_android/app/src/main/java/com/example/iwakiri/sakezukan_android/GuideDetailActivity.java
@@ -1,13 +1,47 @@
 package com.example.iwakiri.sakezukan_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class GuideDetailActivity extends AppCompatActivity {
+public class GuideDetailActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide_detail);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button82:
+                Intent homeIntent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+                break;
+            case R.id.button81:
+                Intent guideIntent = new Intent(getApplicationContext(), GuideActivity.class);
+                startActivity(guideIntent);
+                finish();
+                break;
+            case R.id.button80:
+                Intent tasteIntent = new Intent(getApplicationContext(), TasteActivity.class);
+                startActivity(tasteIntent);
+                finish();
+                break;
+            case R.id.button79:
+                Intent findIntent = new Intent(getApplicationContext(), FindActivity.class);
+                startActivity(findIntent);
+                finish();
+                break;
+            case R.id.button78:
+                Intent helpIntent = new Intent(getApplicationContext(), HelpActivity.class);
+                startActivity(helpIntent);
+                finish();
+                break;
+        }
     }
 }
